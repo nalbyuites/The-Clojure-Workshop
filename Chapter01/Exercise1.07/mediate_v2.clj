91ddc30705d7d5c4bf8edc354166ad35cf04ebab
Ashijit Pramanik <ashijit@mayan.host>
2021-08-04 00:13:09 +0530

    added meditate function v2.0 with calmness-level

---
+++ Chapter01/Exercise1.07/mediate_v2.clj
@@ -0,0 +1,12 @@
+(defn meditate
+  "The function will print a transformation of the string passed as a
+second argument based on the `calmness-level` value."
+  [s calmness-level]
+  (println "Clojure Meditate v2.0")
+  (if (< calmness-level 5)
+    (str (clojure.string/upper-case s) ", I TELL YA!")
+    (if (<= 5 calmness-level 9)
+      (clojure.string/capitalize s)
+      (if (= 10 calmness-level)
+        (clojure.string/reverse s)
+        nil))))
