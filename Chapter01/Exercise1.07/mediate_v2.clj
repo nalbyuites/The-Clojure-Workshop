283903530ba2af1122cf4b55e9b28dd9a0ee98d7
Ashijit Pramanik <ashijit@mayan.host>
2021-08-04 00:20:52 +0530

    added meditate function using cond macro

--- Chapter01/Exercise1.07/mediate_v2.clj
+++ Chapter01/Exercise1.07/mediate_v2.clj
@@ -10,3 +10,13 @@ second argument based on the `calmness-level` value."
       (if (= 10 calmness-level)
         (clojure.string/reverse s)
         nil))))
+
+(defn meditate
+  "The function will print a transformation of the string passed as a
+second argument based on the `calmness-level` value."
+  [s calmness-level]
+  (println "Clojure Meditate v2.0")
+  (cond
+    (< calmness-level 5) (str (clojure.string/upper-case s) ", I TELL YA!")
+    (<= 5 calmness-level 9) (clojure.string/capitalize s)
+    (= 10 calmness-level) (clojure.string/reverse s)))
